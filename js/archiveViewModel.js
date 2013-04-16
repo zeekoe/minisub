ea6ecc75eaac850d365fe80e32db917acce94afa
Trevor Squillario <tsquillario@gmail.com>
2013-04-16 13:23:23 -0400

    3.0 Knockout.js first beta

---
+++ js/archiveViewModel.js
@@ -0,0 +1,155 @@
+﻿/// <reference path="../../js/plugins/knockout-2.2.1.debug.js" />
+/// <reference path="../../js/plugins/knockout.mapping-latest.debug.js" />
+define(['knockout', 'postbox', 'mapping', 'global', 'utils', 'model', 'player'], function (ko, postbox, mapping, global, utils, model, player) {
+    return function archiveViewModel() {
+        var self = this;
+        self.artist = new ko.observableArray([]);
+        self.album = new ko.observableArray([]);
+        self.song = new ko.observableArray([]);
+
+        self.settings = global.settings;
+
+        self.queue = new ko.observableArray([]).syncWith("queue");
+        self.selectedArtist = ko.observable();
+        self.selectedAlbum = ko.observable();
+        self.selectedSongs = new ko.observableArray([]);
+        self.selectSong = function (data, event) {
+            if (self.selectedSongs.indexOf(this) >= 0) {
+                self.selectedSongs.remove(this);
+                this.selected(false);
+            } else {
+                self.selectedSongs.push(this);
+                this.selected(true);
+            }
+        }
+        self.addSongsToQueue = function (data, event) {
+            ko.utils.arrayForEach(self.selectedSongs(), function (item) {
+                self.queue.push(item);
+                item.selected(false);
+            });
+            utils.updateMessage(self.selectedSongs().length + ' Song(s) Added to Queue', true);
+        }
+
+        self.archiveUrl = 'https://archive.org/';
+        self.protocol = 'jsonp';
+
+        self.AllCollections = global.archiveCollections;
+        self.selectedCollectionChange = function (data, event) {
+            var val = event.currentTarget.value;
+            if (self.settings.SavedCollections()) {
+                self.settings.SavedCollections(self.settings.SavedCollections() + ', ' + val)
+            } else {
+                self.settings.SavedCollections(val);
+            }
+            self.artist.push(new model.Artist('', val));
+        };
+        self.getArtists = function (data) {
+            var map = {
+                create: function (options) {
+                    return new model.Artist('', options.data);
+                }
+            };
+            mapping.fromJS(self.settings.SavedCollections().split(","), map, self.artist);
+        };
+        self.getAlbums = function (data, event) {
+            self.selectedArtist(data);
+            var id = event.currentTarget.id;
+            var name = data.name();
+            var map = {
+                create: function (options) {
+                    var song = options.data;
+                    var coverart, starred;
+                    coverart = 'images/albumdefault_50.jpg';
+                    if (parseInt(song.avg_rating) == 5) { starred = true; } else { starred = false; }
+                    //var description = '<b>Details</b><br />';
+                    var description = '<b>Source</b>: ' + song.source + '<br />';
+                    description += '<b>Date</b>: ' + song.date + '<br />';
+                    description += '<b>Rating</b>: ' + song.avg_rating + '<br />';
+                    description += '<b>Downloads</b>: ' + song.downloads + '<br /><br />';
+                    description += song.description;
+                    return new model.Album(song.identifier, null, song.title, null, coverart, null, starred, description);
+                }
+            }
+            //var url = self.settings.BaseURL() + 'advancedsearch.php?q=collection%3A%28GreenskyBluegrass%29%20AND%20format%3A%28mp3%29&fl%5B%5D=avg_rating&fl%5B%5D=collection&fl%5B%5D=date&fl%5B%5D=description&fl%5B%5D=downloads&fl%5B%5D=headerImage&fl%5B%5D=identifier&fl%5B%5D=publicdate&fl%5B%5D=source&fl%5B%5D=subject&fl%5B%5D=title&format=mp3&sort%5B%5D=addeddate+desc&rows=50&page=1&output=json';
+            var url = self.archiveUrl + 'advancedsearch.php?q=collection%3A%28' + name + '%29%20AND%20format%3A%28MP3%29&fl[]=avg_rating,collection,date,description,downloads,headerImage,identifier,publicdate,source,subject,title&sort%5B%5D=date+desc&rows=50&page=1&output=json';
+            $.ajax({
+                url: url,
+                method: 'GET',
+                dataType: self.protocol,
+                timeout: 10000,
+                success: function (data) {
+                    var items = [];
+                    if (data["response"].docs.length > 0) {
+                        items = data["response"].docs;
+                    } else {
+                        items[0] = data["response"].docs;
+                    }
+                    //alert(JSON.stringify(data["response"]));
+                    mapping.fromJS(items, map, self.album);
+                }
+            });
+        };
+        self.openLink = function (action) {
+            alert('foo');
+        }
+        self.getSongs = function (id, action) {
+            self.selectedAlbum(this);
+            var url = self.archiveUrl + 'details/' + id + '?output=json';
+            $.ajax({
+                url: url,
+                method: 'GET',
+                dataType: self.protocol,
+                timeout: 10000,
+                success: function (data) {
+                    var songs = [];
+                    var url, track, title, rating, starred, contenttype, suffix;
+                    var specs = '', coverartthumb = '', coverartfull = '';
+                    var server = data.server;
+                    var dir = data.dir;
+                    if (typeof data.misc.image != 'undefined') {
+                        //coverartthumb = self.settings.BaseURL() + data.misc.image;
+                        coverartthumb = data.misc.image;
+                        coverartfull = coverartthumb;
+                    }
+                    $.each(data.files, function (key, song) {
+                        if (song.format == 'VBR MP3') {
+                            url = 'http://' + server + dir + key;
+                            specs = song.bitrate + 'kbps, ' + song.format.toLowerCase();
+                            if (typeof song.track == 'undefined') { track = '&nbsp;'; } else { track = song.track; }
+                            if (typeof song.title == 'undefined') { title = '&nbsp;'; } else { title = song.title; }
+                            songs.push(new model.Song(song.id, song.album, song.track, title, song.creator, '', song.album, '', coverartthumb, coverartfull, utils.timeToSeconds(song.length), '', '', 'mp3', specs, url, 0, ''));
+                        }
+                    });
+                    if (action == 'add') {
+                        ko.utils.arrayForEach(songs, function (item) {
+                            self.queue.push(item);
+                        });
+                        utils.updateMessage(self.queue().length + ' Song(s) Added to Queue', true);
+                    } else if (action == 'play') {
+                        self.queue(songs);
+                        var next = self.queue()[0];
+                        player.playSong(false, next);
+                        utils.updateMessage(self.queue().length + ' Song(s) Added to Queue', true);
+                    } else {
+                        self.song(songs);
+                    }
+                }
+            });
+        };
+        self.selectAll = function (data, event) {
+            ko.utils.arrayForEach(self.song(), function (item) {
+                self.selectedSongs.push(item);
+                item.selected(true);
+            });
+        }
+        self.selectNone = function (data, event) {
+            ko.utils.arrayForEach(self.song(), function (item) {
+                self.selectedSongs([]);
+                item.selected(false);
+            });
+        }
+
+        // Init
+        self.getArtists();
+    }
+});
---
+++ js/main.js
@@ -0,0 +1,176 @@
+﻿require.config({
+    paths: {
+        'knockout': 'plugins/knockout-2.2.1',
+        'mapping': 'plugins/knockout.mapping-latest',
+        'postbox': 'plugins/knockout-postbox.min',
+        'jquery': 'plugins/jquery-1.7.2.min',
+        'jqueryui': 'plugins/jquery-ui-1.8.20.min',
+        'jquery.cookie': 'plugins/jquery.cookie',
+        'jquery.fancybox': 'plugins/fancybox/jquery.fancybox.pack',
+        'jquery.scrollTo': 'plugins/jquery.scrollTo.min',
+        'sammy': 'plugins/sammy-latest.min',
+        'domReady': 'plugins/domReady',
+        'jplayer': 'jplayer/jquery.jplayer.min',
+        'splitter': 'plugins/splitter',
+        'model': 'model',
+        'global': 'global',
+        'utils': 'utils',
+        'player': 'player'
+    },
+    shim: {
+        'jquery.cookie': {
+            deps: ['jquery']
+        },
+        'jquery.fancybox':{
+            deps: ['jquery']
+        },
+        'jquery.scrollTo':{
+            deps: ['jquery']
+        },
+        "jqueryui": {
+            exports: "$",
+            deps: ['jquery']
+        },
+        'sammy': {
+            deps: ['jquery'],
+            exports: 'Sammy'
+        },
+        'jplayer': {
+            deps: ['jquery'],
+            exports: 'jPlayer'
+        },
+        'splitter': {
+            deps: ['jquery']
+        },
+    },
+    baseUrl: "js"
+}); 
+require(['jquery', 'knockout', 'sammy', 'global', 'utils', 'mainViewModel', 'subsonicViewModel', 'archiveViewModel', 'player', 'jqueryui', 'jquery.fancybox', 'splitter', 'domReady!'], function ($, ko, Sammy, global, utils, mainViewModel, subsonicViewModel, archiveViewModel, player) {
+    var self = this;
+
+    function resizeSplitter(el) {
+        $(el).trigger("resize");
+    }
+    $('#SubsonicAlbums').splitter({
+        type: "v",
+        sizeLeft: 150,
+        cookie: "splitter1",
+        resizeToWidth: true
+    });
+    $('#SubsonicAlbumsRight').splitter({
+        type: "v",
+        cookie: "splitter2",
+        resizeToWidth: true
+    });
+    $('#SubsonicArtists').resize(function() {
+        resizeSplitter('#SubsonicAlbumsRight');
+    });
+    $('#ArchiveAlbums').splitter({
+        type: "v",
+        cookie: "splitter3",
+        resizeToWidth: true
+    });
+    $('#ArchiveAlbumsRight').splitter({
+        type: "v",
+        cookie: "splitter4",
+        resizeToWidth: true
+    });
+    $('#ArchiveArtists').resize(function() {
+        resizeSplitter('#ArchiveAlbumsRight');
+    });
+
+    $('.noselect').disableSelection();
+
+    $.ajaxSetup({
+        'beforeSend': function () {
+            $("#loading").show();
+        },
+        'complete': function () {
+            $("#loading").hide();
+        }
+    });
+
+    $("a#coverartimage").fancybox({
+        beforeShow : function() {
+            //this.title = $('#songdetails_artist').html();
+        },
+        afterLoad : function() {
+            //this.inner.prepend( '<h1>1. My custom title</h1>' );
+            //this.content = '<h1>2. My custom title</h1>';
+        },
+        hideOnContentClick: true,
+        type: 'image',
+        openEffect: 'none',
+        closeEffect: 'none',
+    });
+
+    $('#audiocontainer .scrubber').mouseover(function (e) {
+        $('.audiojs .scrubber').stop().animate({ height: '8px' });
+    });
+    $('#audiocontainer .scrubber').mouseout(function (e) {
+        $('.audiojs .scrubber').stop().animate({ height: '4px' });
+    });
+
+    // JQuery UI Sortable - Drag and drop sorting
+    var fixHelper = function (e, ui) {
+        ui.children().each(function () {
+            $(this).width($(this).width());
+        });
+        return ui;
+    };
+    $("#tabQueue ul.songlist").sortable({
+        helper: fixHelper
+    }).disableSelection();
+
+    // Custom Binding for (stopBinding: true)
+    ko.bindingHandlers.stopBinding = {
+        init: function () {
+            return { controlsDescendantBindings: true };
+        }
+    };
+    //KO 2.1, now allows you to add containerless support for custom bindings
+    //ko.virtualElements.allowedBindings.stopBinding = true;
+
+    // Custom Binding for Enter Key (Use on input, returnKey: function)
+    ko.bindingHandlers.returnKey = {
+        init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
+            ko.utils.registerEventHandler(element, 'keydown', function (evt) {
+                if (evt.keyCode === 13) {
+                    evt.preventDefault();
+                    evt.target.blur();
+                    valueAccessor().call(viewModel);
+                }
+            });
+        }
+    };
+
+    ko.bindingHandlers.templateWithContext = {
+        init: function() {
+            return ko.bindingHandlers.template.init.apply(this, arguments);
+        },
+        update: function(element, valueAccessor) {
+            var options = ko.utils.unwrapObservable(valueAccessor());
+
+            if (options.context) {
+                options.context.data = options.data;
+                options.data = options.context;  
+                delete options.context;
+            }
+
+            ko.bindingHandlers.template.update.apply(this, arguments);
+        } 
+    };           
+
+    ko.applyBindings(new mainViewModel());
+    ko.applyBindings(subsonicViewModel, $('#tabLibrary')[0]);
+    ko.applyBindings(new archiveViewModel(), $('#tabArchive')[0]);
+
+    // Variable Init
+    if (global.settings.SaveTrackPosition()) {
+        player.loadTrackPosition();
+    }
+    if (global.settings.Theme() == 'Dark') {
+        utils.switchTheme(global.settings.Theme());
+    }
+   
+});
